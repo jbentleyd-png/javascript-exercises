cd6e12f4cd776a3be87ccd50240d9785dab71b3e
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 21:00:05 -0700

    Finished 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  let message = "";
+  for (i = 0; i < num; i++) {
+    message = message + string;
+  }
+  return message;
 };
+
+console.log(repeatString("hey", 3));
+console.log(repeatString("hey", 0));
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
