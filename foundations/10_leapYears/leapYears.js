58f8f0e2c28245e6ce39ad822e516931f28db111
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 23:22:01 -0700

    Solved 10.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
 };
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
