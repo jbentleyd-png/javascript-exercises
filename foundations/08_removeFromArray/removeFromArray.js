812eac629cd6c01b78960e215a4d05b0c23e5f78
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 22:20:43 -0700

    Solved 08
    Turns out I worked a bit too hard.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toBeRemoved) {
+  let filtered = array;
+  for (let i of toBeRemoved) {
+    filtered = filtered.filter((item) => item !== i);
+  }
+  return filtered;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 3, "4"));
+
+/* THIS would have been the faster way: 
+const removeFromArray = function (array, ...toBeRemoved) {
+  return array.filter(item => !toBeRemoved.includes(item));
+};
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
