a44107293acc53933d390d439d2e0987853ec548
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 23:38:58 -0700

    Solved 11.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  const decimalC = (F - 32) * (5 / 9);
+  const C = Math.round(decimalC * 10) / 10;
+  return C;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(50) + "C");
+
+const convertToFahrenheit = function (C) {
+  const decimalF = C * (9 / 5) + 32;
+  const F = Math.round(decimalF * 10) / 10;
+  return F;
 };
+
+console.log(convertToFahrenheit(20) + "F");
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
