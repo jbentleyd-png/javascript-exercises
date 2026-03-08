e0d5f5f891362865f3d6c268021a706996090a83
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 23:01:55 -0700

    Solved 09.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  if (
+    firstNum < 0 ||
+    secondNum < 0 ||
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(secondNum)
+  ) {
+    return "ERROR";
+  }
 
+  const difference = Math.abs(firstNum - secondNum);
+  const smallerNumber = firstNum > secondNum ? secondNum : firstNum;
+
+  let output = smallerNumber;
+  let nextNumber = smallerNumber + 1;
+  for (i = 0; i < difference; i++) {
+    output += nextNumber;
+    nextNumber++;
+  }
+
+  return output;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
