3d0934199c1411e9589b0e36d377ecb2e13e6975
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-07 21:17:10 -0700

    Solved 07.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let output = [];
+  for (i = 0; i < input.length; i++) {
+    output[i] = input.charAt(i);
+  }
+  output.reverse();
+  output = output.join("");
+  return output;
 };
+
+console.log(reverseString("dog"));
+console.log(reverseString("goat"));
 
 // Do not edit below this line
 module.exports = reverseString;
