f9f5c04e788eed1b600b76815ae1650c066a0254
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-24 11:41:02 -0600

    Solved 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num == 1 || num == 2) {
+    return 1;
+  } else if (num == 0) {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  }
 
+  const fibArr = [1, 1];
+  let nextNum;
+  while (fibArr.length < num) {
+    nextNum = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
+    fibArr.push(nextNum);
+  }
+  return nextNum;
 };
 
 // Do not edit below this line
