617445427104ca7cabcf7fda86440c74596da59f
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-24 11:04:36 -0600

    solved 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const cleanWord = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const wordArray = Array.from(cleanWord);
+  wordArray.reverse();
+  let reverseWord = wordArray.join("");
 
+  if (cleanWord == reverseWord) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
