6cd3c9129c9cb3ec74b4358afa01b3f4d01662c0
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-24 10:21:44 -0600

    solved 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  let total = 0;
+  for (n in numArray) {
+    total += numArray[n];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  let product = 1;
+  for (n in numArray) {
+    product *= numArray[n];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0 || number == 1) {
+    return 1;
+  } else {
+    let i = number;
+    do {
+      i--;
+      number *= i;
+    } while (i > 1);
+    return number;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
