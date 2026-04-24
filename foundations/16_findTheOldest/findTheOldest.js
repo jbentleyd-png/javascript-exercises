443bf5f60b4555f55bddbf2552b16dc0d8a322ab
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-24 15:44:44 -0600

    finished 16
    {} suck

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  let age;
+  const currentYear = new Date().getFullYear();
+  for (person in peopleArray) {
+    if (peopleArray[person].yearOfDeath == undefined) {
+      age = currentYear - peopleArray[person].yearOfBirth;
+    } else {
+      age = peopleArray[person].yearOfDeath - peopleArray[person].yearOfBirth;
+    }
+    peopleArray[person].age = age;
+  }
 
+  peopleArray.sort((a, b) => a.age - b.age); //sorts by age with arrow function as criterion
+  console.log(JSON.stringify(peopleArray, null, 2)); //needs to be stringy to see object contents without a proper debugger
+
+  return peopleArray.at(-1);
 };
 
 // Do not edit below this line
